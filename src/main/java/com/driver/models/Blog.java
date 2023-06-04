0e5eb710191ddc70ef58cf5d7e5fe918ed4aca41
Shubham Jauhari <90859295+Jauhari-01@users.noreply.github.com>
2023-06-04 18:08:44 +0000

    Done assignment

---
+++ src/main/java/com/driver/models/Blog.java
@@ -0,0 +1,100 @@
+package com.driver.models;
+
+import javax.persistence.*;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+
+
+@Entity
+@Table(name = "blog")
+public class Blog{
+    @Id 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Coloumn(name = "title")
+    private String title;
+
+    @Coloumn(name = "content")
+    private String content;
+
+    
+    @Coloumn(name = "pub_date")
+    @CreationTimestamp
+    private Date pubDate;
+
+    //Constructors
+    public Blog() {
+
+    }
+
+    public Blog(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public Blog(String title, String content, Date pubDate) {
+        this.title = title;
+        this.content = content;
+        this.pubDate = pubDate;
+    }
+
+    //Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getPubDate() {
+        return pubDate;
+    }
+
+    public void setPubDate(Date pubDate) {
+        this.pubDate = pubDate;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public List<Image> getImageList() {
+        return imageList;
+    }
+
+    public void setImageList(List<Image> imageList) {
+        this.imageList = imageList;
+    }
+
+    //child - parent relation ship (child for user)
+    @ManyToOne
+    @JoinColumn
+    User user;
+
+    //parent to child (parent for images)
+    @OneToMany(mappedBy="blog",cascade = CascadeType.ALL)
+    List<Image> imageList = new ArrayList<>();
+}
--- src/main/java/com/driver/services/ImageService.java
+++ src/main/java/com/driver/services/ImageService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ImageService {
@@ -17,15 +18,47 @@ public class ImageService {
 
     public Image addImage(Integer blogId, String description, String dimensions){
         //add an image to the blog
+        Optional<Blog> optionalBlog = blogRepository2.findById(blogId);
+        Blog blog = optionalBlog.get();
 
+        Image image = new Image(description,dimensions);
+        image.setBlog(blog);
+
+        List<Image> imageList = blog.getImageList();
+        imageList.add(image);
+        blogRepository2.save(blog);
+        return image;
     }
 
     public void deleteImage(Integer id){
-
+        imageRepository2.deleteById(id);
     }
 
     public int countImagesInScreen(Integer id, String screenDimensions) {
         //Find the number of images of given dimensions that can fit in a screen having `screenDimensions`
+        Optional<Image> optionalImage = imageRepository2.findById(id);
 
+        Image image = optionalImage.get();
+
+        String dimension = image.getDescription();
+
+        //change the screen dimension to the integers value
+        String str[] = screenDimensions.split("X");
+
+        int heightScreen = Integer.valueOf(str[0]);
+        int widthScreen = Integer.valueOf(str[1]);
+
+        // change the current Image dimension to the Integers value
+        String strImage[] = dimension.split("X");
+
+        int heightImage = Integer.valueOf(strImage[0]);
+        int widthImage = Integer.valueOf(strImage[1]);
+
+
+        int heightRatio = heightScreen/heightImage;
+        int widthRatio = widthScreen/widthImage;
+
+        int count = heightRatio * widthRatio;
+        return count;
     }
 }
